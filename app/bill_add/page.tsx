265a819a8337afa8cd6b14813f82943e3ebd3e7c
silencewul <wulsilence@gmail.com>
2023-11-16 18:02:09 +0800

    up

--- app/bill_add/page.tsx
+++ app/bill_add/page.tsx
@@ -3,7 +3,7 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import axios from "axios";
 import {useRouter} from "next/navigation";
-import { useEffect, useState } from 'react'
+import { useEffect, useState, } from 'react'
 
 export default function AddBill() {
     const router = useRouter()
@@ -14,20 +14,29 @@ export default function AddBill() {
     const [money,setMoney] = useState(0);
     const [note,setNote] = useState('');
 
-
     useEffect(() => {
-        fetchData();
-      }, []);
+        fetchData(kind);
+      }, [kind]);
 
-    const fetchData = async () => {
+    const fetchData = async (kind) => {
     try {
-        const response = await axios.get('http://127.0.0.1:3001/api/category');
+        const response = await axios.get('http://127.0.0.1:3001/api/category?k='+kind);
         const data = response.data;
         setCate(data.data);
     } catch (error) {
         console.error('请求错误:', error);
     }
     };
+
+    function setKindClick(e,k) {
+      e.preventDefault();
+        setKind( k )
+        console.log("k-------"+k)
+        console.log("kind-------"+kind)
+        if (k != kind) {
+            fetchData(k)
+        }
+    }
 
     async function submit() {
         let data = {
@@ -53,7 +62,7 @@ export default function AddBill() {
   return <div className="lg:flex sm:items-center sm:justify-center">
     <div className="mt-5 flex lg:ml-4 lg:mt-0">
     <span className=" sm:block mx-auto">
-      <button type="button" onClick={() => setKind(1) }
+      <button type="button" onClick= {(e) => setKindClick(e,1)} 
               className={(kind == 1 ? "bg-red-300":"bg-white")+" inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300"}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -63,7 +72,7 @@ export default function AddBill() {
     </span>
 
       <span className="mx-auto sm:block">
-      <button type="button" onClick={() => setKind(2) }
+      <button type="button" onClick={(e) => setKindClick(e,2) }
               className={(kind == 2 ? "bg-green-300":"bg-white")+" inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300"}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -77,12 +86,15 @@ export default function AddBill() {
     <p className="mt-5 pt-5 px-2 tracking-tight text-gray-900">收支类型</p>
 
     <div className="justify-left grid grid-cols-4">
-      <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200">
+    {cate.map((c)=>(
+        <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-1 mx-auto flex h-8 w-8">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d={c.icon} />
           </svg>
-          <p className="text-center">购物</p>
+          <p className="text-center">{c.title}</p>
       </div>
+    ))}
+      
 
         <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200 bg-indigo-400">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-1 mx-auto flex h-8 w-8">
@@ -90,47 +102,7 @@ export default function AddBill() {
             </svg>
             <p className="text-center">餐饮</p>
         </div>
-        <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-1 mx-auto flex h-8 w-8">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
-            </svg>
-            <p className="text-center">送礼</p>
-        </div>
-        <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-1 mx-auto flex h-8 w-8">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-            </svg>
-            <p className="text-center">话费</p>
-
-        </div>
-        <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-1 mx-auto flex h-8 w-8">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
-            </svg>
-            <p className="text-center">股票</p>
-
-        </div>
-        <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-1 mx-auto flex h-8 w-8">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-            </svg>
-            <p className="text-center">房租</p>
-
-        </div>
-        <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-1 mx-auto flex h-8 w-8">
-                <path strokeLinecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-            </svg>
-            <p className="text-center">电影</p>
-
-        </div>
-        <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-1 mx-auto flex h-8 w-8">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M9 7.5l3 4.5m0 0l3-4.5M12 12v5.25M15 12H9m6 3H9m12-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <p className="text-center">工资</p>
-
-        </div>
+        
 
     </div>
 
