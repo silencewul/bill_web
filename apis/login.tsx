2180a30b33357622f70d1b91c6dbc9cb11f6c7da
silencewul <wulsilence@gmail.com>
2023-11-17 18:03:35 +0800

    up

--- apis/login.tsx
+++
@@ -1,3 +0,0 @@
-export default function Login(data:any) {
-    console.log(data)
-}
--- app/bill_add/page.tsx
+++ app/bill_add/page.tsx
@@ -10,15 +10,15 @@ export default function AddBill() {
     const [value, onChange] = useState(new Date());
     const [kind,setKind] = useState(1);
     const [cate,setCate] = useState([]);
-    const [selectCate,setSelectCate] = useState(1);
+    const [selectCate,setSelectCate] = useState(0);
     const [money,setMoney] = useState(0);
     const [note,setNote] = useState('');
 
     useEffect(() => {
-        fetchData(kind);
+        fetchData();
       }, [kind]);
 
-    const fetchData = async (kind) => {
+    const fetchData = async () => {
     try {
         const response = await axios.get('http://127.0.0.1:3001/api/category?k='+kind);
         const data = response.data;
@@ -31,10 +31,9 @@ export default function AddBill() {
     function setKindClick(e,k) {
       e.preventDefault();
         setKind( k )
-        console.log("k-------"+k)
-        console.log("kind-------"+kind)
+        setSelectCate(0)
         if (k != kind) {
-            fetchData(k)
+            // fetchData(k)
         }
     }
 
@@ -87,7 +86,7 @@ export default function AddBill() {
 
     <div className="justify-left grid grid-cols-4">
     {cate.map((c)=>(
-        <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200">
+        <div onClick={() => setSelectCate(c.id)} className={(selectCate == c.id ? "bg-indigo-400":"")+" h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200"}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-1 mx-auto flex h-8 w-8">
               <path strokeLinecap="round" strokeLinejoin="round" d={c.icon} />
           </svg>
@@ -96,12 +95,7 @@ export default function AddBill() {
     ))}
       
 
-        <div className="h-16 w-16  items-center  ml-3 mt-4 rounded-md border border-gray-200 bg-indigo-400">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-1 mx-auto flex h-8 w-8">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 8.25v-1.5m0 1.5c-1.355 0-2.697.056-4.024.166C6.845 8.51 6 9.473 6 10.608v2.513m6-4.87c1.355 0 2.697.055 4.024.165C17.155 8.51 18 9.473 18 10.608v2.513m-3-4.87v-1.5m-6 1.5v-1.5m12 9.75l-1.5.75a3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0L3 16.5m15-3.38a48.474 48.474 0 00-6-.37c-2.032 0-4.034.125-6 .37m12 0c.39.049.777.102 1.163.16 1.07.16 1.837 1.094 1.837 2.175v5.17c0 .62-.504 1.124-1.125 1.124H4.125A1.125 1.125 0 013 20.625v-5.17c0-1.08.768-2.014 1.837-2.174A47.78 47.78 0 016 13.12M12.265 3.11a.375.375 0 11-.53 0L12 2.845l.265.265zm-3 0a.375.375 0 11-.53 0L9 2.845l.265.265zm6 0a.375.375 0 11-.53 0L15 2.845l.265.265z" />
-            </svg>
-            <p className="text-center">餐饮</p>
-        </div>
+
         
 
     </div>
